c2e6efdbf2fd920f5f91b30da6b051253b5cf7fa
Eric Dobbertin <eric@dairystatedesigns.com>
2013-07-06 15:28:18 -0500

    add initial files

---
+++ handlebars-scripts.js
@@ -0,0 +1,46 @@
+Handlebars.registerHelper("facebookScript", function(appId) {
+    appId = appId && appId.length ? "&appId=" + appId : "";
+    Meteor.startup(function() {
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id))
+                return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/all.js#xfbml=1" + appId;
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    });
+    return new Handlebars.SafeString('<div id="fb-root"></div>');
+});
+
+Handlebars.registerHelper("googleTagManagerScript", function(containerId) {
+    if (! containerId || ! containerId.length) {
+        return "";
+    }
+    Meteor.startup(function() {
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', containerId);
+    });
+    return new Handlebars.SafeString('<noscript><iframe src="//www.googletagmanager.com/ns.html?id=' + containerId + '" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>');
+});
+
+Handlebars.registerHelper("googlePlusOneScript", function() {
+    Meteor.startup(function() {
+        (function() {
+            var po = document.createElement('script');
+            po.type = 'text/javascript';
+            po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(po, s);
+        })();
+    });
+});
---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+  summary: "Handlebars helpers to easily insert popular third-party script blocks in a way that works with Meteor's quirks."
+});
+
+Package.on_use(function(api) {
+  api.use(['underscore', 'templating']);
+  api.add_files(['handlebars-scripts.js'], ['client']);
+});
